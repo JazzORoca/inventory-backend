090fe44f358fc7e116c65ceab8ae7751b1348fe3
JazzORoca <jornellaroca@gmail.com>
2023-10-25 14:07:01 -0300

    buscar categoria por id

--- src/main/java/com/company/inventory/services/CategoryServicesImpl.java
+++ src/main/java/com/company/inventory/services/CategoryServicesImpl.java
@@ -33,5 +33,30 @@ public class CategoryServicesImpl implements ICategoryService{
 		return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.OK);
 		
 	}
+	@Transactional(readOnly=true)
+	public ResponseEntity<CategoryResponseRest> searchById(Long id) {
+		CategoryResponseRest response=new CategoryResponseRest();
+		List<Category>list=new ArrayList<>();
+		try{
+			//devuelve un objeto optional
+			Optional<Category>category=categoryDao.findById(id);
+			if(category.isPresent()) {
+				list.add(category.get());
+				response.getCategoryResponse().setCategory(list);
+
+				response.setMetadata("Respuesta ok", "00", "categoria encontrada");
+			}else {
+				response.setMetadata("Respuesta nok", "-1", "Error categoria no encotrada");
+				
+				return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.NOT_FOUND);
+			}
+		}catch(Exception e) {
+
+			response.setMetadata("Respuesta nok", "-1", "Error al consultar");
+			e.getStackTrace();
+			return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		return new ResponseEntity<CategoryResponseRest>(response,HttpStatus.OK);
+	}
 
 }
